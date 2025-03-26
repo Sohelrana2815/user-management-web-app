b89e8cffb8f3e227dae5230d71825264796f6376
Sohelrana2815 <sohel152302@gmail.com>
2025-03-26 19:49:05 +0600

    done

--- frontend/src/components/privateRoute/PrivateRoute.jsx
+++ frontend/src/components/privateRoute/PrivateRoute.jsx
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router";
-import Swal from "sweetalert2";
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token");
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-
-  useEffect(() => {
-    if (!token) {
-      const timer = setTimeout(() => {
-        setShouldRedirect(true);
-      }, 1500); // 1500ms delay (1.5 seconds)
-      return () => clearTimeout(timer);
-    }
-  }, [token]);
-
-  if (!token && shouldRedirect) {
-    Swal.fire({
-      title: "Blocked!",
-      text: "User has been blocked!",
-      icon: "success",
-    });
+  if (!token) {
+    // No token => not authenticated => redirect
     return <Navigate to="/" replace />;
   }
-
-  if (!token) {
-    // Optionally, you can render a loading state while waiting
-    return <div>Loading...</div>;
-  }
-
-  // Render the protected page if authenticated
-  return children;
+  return children; // Render the protected page
 };
 
 export default PrivateRoute;
